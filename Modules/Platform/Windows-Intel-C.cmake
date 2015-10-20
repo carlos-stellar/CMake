0c9fb2f62974616ac40ef6e25da3b8053b54dc67
Brad King <brad.king@kitware.com>
2015-10-20 13:43:17 -0400

    WIP: Use deps=gcc for Intel Compiler on Windows
    TODO: Does not work with spaces in the path because icl generates
    incorrect info (fails to escape spaces).

--- Modules/Platform/Windows-Intel-C.cmake
+++ Modules/Platform/Windows-Intel-C.cmake
@@ -1,2 +1,4 @@
 include(Platform/Windows-Intel)
 __windows_compiler_intel(C)
+set(CMAKE_NINJA_DEPTYPE_C gcc)
+set(CMAKE_DEPFILE_FLAGS_C "-QMMD -QMT <OBJECT> -QMF <DEPFILE>")
--- Modules/Platform/Windows-Intel-CXX.cmake
+++ Modules/Platform/Windows-Intel-CXX.cmake
@@ -1,3 +1,5 @@
 include(Platform/Windows-Intel)
 set(_COMPILE_CXX " /TP")
 __windows_compiler_intel(CXX)
+set(CMAKE_NINJA_DEPTYPE_CXX gcc)
+set(CMAKE_DEPFILE_FLAGS_CXX "-QMMD -QMT <OBJECT> -QMF <DEPFILE>")
